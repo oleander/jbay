1bd4626812bb48029f6949e6b29909d34a89cea9
cambr <daniel.tell4@gmail.com>
2013-02-26 17:36:43 +0100

    setup and takedown agents

--- JADE/src/Mediator.java
+++ JADE/src/Mediator.java
@@ -2,10 +2,11 @@ import jade.core.Agent;
 
 public class Mediator extends Agent {
   static final long serialVersionUID = 1;
+  @Override
   protected void setup() { 
     System.out.println("Hello! Buyer-agent " + getAID().getName() + " is ready.");
   }
-
+  @Override
   protected void takeDown(){
     System.out.println("Mediator " + getAID().getName() + " terminating.");
   }
