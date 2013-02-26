445e5428707868d3dd7d143e236019da0db1cbbb
Linus Oleander <linus@oleander.nu>
2013-02-26 17:31:38 +0100

    Add BidderBehaviour classes

---
+++ JADE/src/NotifyNotHighestBidderBehaviour.java
@@ -0,0 +1,18 @@
+import jade.core.behaviours.Behaviour;
+
+
+public class NotifyNotHighestBidderBehaviour extends Behaviour {
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
