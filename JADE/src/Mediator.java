a2bcb7df2692608a7f9c58aed2a6c837d2bef7bf
Linus Oleander <linus@oleander.nu>
2013-02-26 17:00:32 +0100

    Add more examples

--- JADE/src/Mediator.java
+++ JADE/src/Mediator.java
@@ -1,8 +1,12 @@
 import jade.core.Agent;
 
 public class Mediator extends Agent {
-	static final long serialVersionUID = 1;
-	protected void setup() { 
-		System.out.println("Hello! Buyer-agent " + getAID().getName() + " is ready.");
-	}
+  static final long serialVersionUID = 1;
+  protected void setup() { 
+    System.out.println("Hello! Buyer-agent " + getAID().getName() + " is ready.");
+  }
+
+  protected void takeDown(){
+    System.out.println("Mediator " + getAID().getName() + " terminating.");
+  }
 }
