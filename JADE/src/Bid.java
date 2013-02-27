195ffec3e6936efd27d8ed045cd0cfac37e48226
Linus Oleander <linus@oleander.nu>
2013-02-27 20:04:49 +0100

    Implement bid related methods in auction class

--- JADE/src/Bid.java
+++ JADE/src/Bid.java
@@ -2,34 +2,27 @@ import java.util.Date;
 
 
 public class Bid {
-	
-	private final int auctionID;
-	private final int amount;
-	private final Buyer bidder;
-	private final Date date;
-	
-	
-	public Bid(int auctionID, int amount, Buyer bidder, Date date) {
-
-		this.auctionID = auctionID;	
-		this.amount = amount;
-		this.bidder = bidder;
-		this.date = date;
-	}	
-	public int getAuctionID() {
-		return auctionID;
-	}
-	public int getAmount() {
-		return amount;
-	}
-	public Buyer getBidder() {
-		return bidder;
-	}
-	public Date getDate() {
-		return date;
-	}
-	
-	
-	
-
+    private final int auctionID;
+    private final int amount;
+    private final Buyer bidder;
+    private final Date date;
+    
+    public Bid(int auctionID, int amount, Buyer bidder, Date date) {
+        this.auctionID = auctionID; 
+        this.amount = amount;
+        this.bidder = bidder;
+        this.date = date;
+    }
+    public int getAuctionID() {
+        return auctionID;
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public Buyer getBidder() {
+        return bidder;
+    }
+    public Date getDate() {
+        return date;
+    }
 }
