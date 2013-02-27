8ce84be25803e6a1f0c23dc7dc6ac0b9ac4dfb99
Linus Oleander <linus@oleander.nu>
2013-02-27 20:27:03 +0100

    Fix indentaiton

--- JADE/src/MakeBidBehaviour.java
+++ JADE/src/MakeBidBehaviour.java
@@ -10,19 +10,19 @@ import jade.lang.acl.ACLMessage;
 public class MakeBidBehaviour extends Behaviour {
 
   @Override
-  public void action() {	  
-	  ACLMessage senderMessage = new ACLMessage(ACLMessage.REQUEST); 
-	  senderMessage.addReceiver(new AID("mediator", AID.ISLOCALNAME));
-	  Date timeStamp = new Date(System.currentTimeMillis());
-	  Bid bid = new Bid(0, 10, (Buyer) myAgent, timeStamp);
-	  try {
-		senderMessage.setContentObject((Serializable) bid);
-		myAgent.send(senderMessage);
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
-	  
+  public void action() {      
+      ACLMessage senderMessage = new ACLMessage(ACLMessage.REQUEST); 
+      senderMessage.addReceiver(new AID("mediator", AID.ISLOCALNAME));
+      Date timeStamp = new Date(System.currentTimeMillis());
+      Bid bid = new Bid(0, 10, (Buyer) myAgent, timeStamp);
+      try {
+        senderMessage.setContentObject((Serializable) bid);
+        myAgent.send(senderMessage);
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+      
     
   }
 
