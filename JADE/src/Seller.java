d9a74c00e7df3a8493e1516625d4d9887c9dadc8
cambr <daniel.tell4@gmail.com>
2013-02-28 17:53:10 +0100

    seller

--- JADE/src/Seller.java
+++ JADE/src/Seller.java
@@ -6,6 +6,11 @@ public class Seller extends Agent {
 
     @Override
     protected void setup(){
+    	
+//    	String description =  (String) getArguments()[0];
+//    	int minPrice = (int) getArguments()[1];
+//    	String type 
+    	
         // Create new auction and listen for confirmation
         this.addBehaviour(new RequestCreateAuctionBehaviour());
 
